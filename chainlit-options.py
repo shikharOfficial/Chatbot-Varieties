edeb9900a27d7a54e243c8311949b59951245aea
shikhar-dita-test <shikhar.rm+test1@metapercept.com>
2024-08-05 15:03:42 +0530

    select option to upload pdfs

---
+++ chainlit-options.py
@@ -0,0 +1,118 @@
+import os
+import time
+from typing import List
+from langchain_community.llms.ctransformers import CTransformers
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.chains.conversational_retrieval.base import (
+    ConversationalRetrievalChain,
+)
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.docstore.document import Document
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import ChatMessageHistory
+
+import chainlit as cl
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+DB_FAISS_PATH = 'vectorstores/db_faiss'
+MODEL_NAME = "TheBloke/Llama-2-7B-Chat-GGML"
+SETTINGS = {
+    'temperature': 0.01,
+    'max_new_tokens': 600,
+    'context_length': 1200
+}
+llm = CTransformers(
+        model=MODEL_NAME,
+        model_type="llama",
+        config=SETTINGS 
+    )
+
+def extract_text_from_pdf(file_path):
+    loader = DirectoryLoader(os.path.dirname(file_path), glob='*.pdf', loader_cls=PyPDFLoader)
+    documents = loader.load()
+    texts = text_splitter.split_documents(documents)
+    return texts
+
+@cl.on_chat_start
+async def on_chat_start():
+    files = None
+
+    # Wait for the user to upload a file
+    while files == None:
+        files = await cl.AskFileMessage(
+            content="Please upload a PDF file to begin!",
+            accept=["application/pdf"],
+            max_size_mb=40,
+            timeout=180,
+        ).send()
+
+    file = files[0]
+
+    msg = cl.Message(content=f"Processing `{file.name}`...")
+    await msg.send()
+
+    texts = extract_text_from_pdf(file.path)
+
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(DB_FAISS_PATH)
+
+    message_history = ChatMessageHistory()
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        output_key="answer",
+        chat_memory=message_history,
+        return_messages=True,
+    )
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm,
+        chain_type="stuff",
+        retriever=db.as_retriever(),
+        memory=memory,
+        return_source_documents=True,
+        verbose=True
+    )
+
+    msg.content = f"Processing `{file.name}` done. You can now ask questions!"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
+    cb = cl.AsyncLangchainCallbackHandler()
+
+    start_time = time.time()  # Start timing
+
+    res = await chain.acall(message.content, callbacks=[cb])
+    answer = res["answer"]
+    source_documents = res["source_documents"]  # type: List[Document]
+
+    end_time = time.time()  # End timing
+    duration = end_time - start_time  # Calculate duration
+
+    text_elements = []  # type: List[cl.Text]
+
+    if source_documents:
+        for source_idx, source_doc in enumerate(source_documents):
+            source_name = f"source_{source_idx}"
+            # Create the text element referenced in the message
+            text_elements.append(
+                cl.Text(content=source_doc.page_content, name=source_name, display="side")
+            )
+        source_names = [text_el.name for text_el in text_elements]
+
+        if source_names:
+            answer += f"\nSources: {', '.join(source_names)}"
+        else:
+            answer += "\nNo sources found"
+
+    answer += f"\nTime taken for query retrieval: {duration:.2f} seconds"
+
+    await cl.Message(content=answer, elements=text_elements).send()
